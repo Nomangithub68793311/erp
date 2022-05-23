fa69342c85fe7e74c35550dd90ea17c052770dda
codecommit <rana.buddy@gmail.com>
2022-05-23 23:52:53 +0600

    added to github

--- HR/views.py
+++ HR/views.py
@@ -19,22 +19,22 @@ def update(request):
     return HttpResponse("from update")
 def delete(request,id):
     return HttpResponse(id)
-# @csrf_exempt    
-# def addData(request):
-   
-#     if request.method == 'POST':
-#      tutorial_data = JSONParser().parse(request)
-#      data_ser=Adddataserializer(data=tutorial_data)
-#     if data_ser.is_valid():
-#         data_ser.save()
-        
-#         return JsonResponse(data_ser.data,safe=False)
-#     return JsonResponse("failed") 
-
 @csrf_exempt    
 def addData(request):
    
     if request.method == 'POST':
      tutorial_data = JSONParser().parse(request)
-    return JsonResponse(tutorial_data,safe=False)
+     data_ser=Adddataserializer(data=tutorial_data)
+    if data_ser.is_valid():
+        data_ser.save()
+        
+        return JsonResponse(data_ser.data,safe=False)
+    return JsonResponse("failed") 
+
+# @csrf_exempt    
+# def addData(request):
+   
+#     if request.method == 'POST':
+#      tutorial_data = JSONParser().parse(request)
+#     return JsonResponse(tutorial_data,safe=False)
                
